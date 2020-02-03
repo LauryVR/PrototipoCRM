6e1653cf689d380d426f4e4811edfe899504d70b
Omar <floresaleman1998@hotmail.com>
2020-02-03 15:44:51 -0600

    cursos

---
+++ Formulario/Formulario/Pages/Cursos.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Formulario
+{
+    public class CursosModel : PageModel
+    {
+        private readonly ILogger<CursosModel> _logger;
+
+        public CursosModel(ILogger<CursosModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
